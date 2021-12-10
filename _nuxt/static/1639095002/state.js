fa88fefdb2b684f2a84f9bd12ff4410aeee8325d
David Ha <David.ha0908@gmail.com>
2021-12-09 17:10:05 -0700

    Updates

---
+++ _nuxt/static/1639095002/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCodewell-Unifeed-Blog-with-NuxtJS\u002F_nuxt\u002Fstatic\u002F1639095002",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FCodewell-Unifeed-Blog-with-NuxtJS\u002F",assetsPath:"\u002FCodewell-Unifeed-Blog-with-NuxtJS\u002F_nuxt\u002F",cdnURL:a}}}}(null));
